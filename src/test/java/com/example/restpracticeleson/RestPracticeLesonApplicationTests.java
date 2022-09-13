bfa802aa7199a3a98dd6ba8598c0321b1f7c06cf
vladyslavkosenko <vaduha91@gmail.com>
2022-09-13 13:36:50 +0300

    first commit

---
+++ src/test/java/com/example/restpracticeleson/RestPracticeLesonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restpracticeleson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestPracticeLesonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
